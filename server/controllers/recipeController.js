94a97db754b77f2a9d6f35fd00e909ccb2d20519
canicemiachael <canicemiachael@gmail.com>
2023-02-11 18:58:35 +0100

    Updated

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,7 +1,71 @@
-
-
+require("../models/database");
+const Category = require("../models/Category");
 
 // GET / homepage
-exports.homepage = async(req, res) => {
-    res.render('index', { title: 'Cooking Blog - Homepage' });
+exports.homepage = async (req, res) => {
+    try {
+        const limitNumber = 5;
+        const categories = await Category.find({}).limit(limitNumber);
+
+        res.render("index", { title: "Cooking Blog - Homepage", categories });
+    } catch (error) {
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany([
+      {
+        name: "Thai",
+        image: "thai-food.jpg",
+      },
+      {
+        name: "american",
+        image: "american-food.jpg",
+      },
+      {
+        name: "chinese",
+        image: "chinese-food.jpg",
+      },
+      {
+        name: "mexican",
+        image: "mexican-food.jpg",
+      },
+      {
+        name: "indian",
+        image: "indian-food.jpg",
+      },
+      {
+        name: "spanish",
+        image: "spanish-food.jpg",
+      },
+    ]);
+  } catch (error) {
+    console.log("err ", +error);
+  }
 }
+
+// insertDummyCategoryData();
