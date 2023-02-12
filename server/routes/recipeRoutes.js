ea465fad4aae7d0d645d63f532446ca5c34f047e
canicemiachael <canicemiachael@gmail.com>
2023-02-12 18:53:14 +0100

    ref: add new route to call the categories route

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,6 +4,7 @@ const recipeController = require('../controllers/recipeController');
 
 // App Routes
 router.get('/', recipeController.homepage);
+router.get('/categories', recipeController.exploreCategories);
 
 
 module.exports = router;
