f6af7ed8382302fc6ec54d1089046b27a584c791
canicemichael <canicemichael@gmail.com>
2023-02-21 13:51:58 +0100

    chore: writing query to filter different category of recipe

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -7,8 +7,14 @@ exports.homepage = async (req, res) => {
     try {
         const limitNumber = 5;
         const categories = await Category.find({}).limit(limitNumber);
+        const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+        const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber)
+        const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+        const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
 
-        res.render("index", { title: "Cooking Blog - Homepage", categories });
+        const food = { latest, thai, american, chinese };
+
+        res.render("index", { title: "Cooking Blog - Homepage", categories, food });
     } catch (error) {
         res.status(500).send({message: error.message || "Error Occured"});
     }
