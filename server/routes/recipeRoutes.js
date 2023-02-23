9cd3a527bdaeea4d74a15e20f1935de439682e6c
canicemichael <canicemichael@gmail.com>
2023-02-23 13:52:28 +0100

    feat: add three routes

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,7 +4,10 @@ const recipeController = require('../controllers/recipeController');
 
 // App Routes
 router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe);
 router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
 
 
 module.exports = router;
