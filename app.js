5fb03b4cda468de094d830a90a9e1c95b39d9801
canicemichael <canicemichael@gmail.com>
2023-02-24 14:05:24 +0100

    add: add upload n flash dependencies

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
+// The dependencies needed to make file uploads work
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
 const app = express();
 const port = process.env.PORT || 3002;
 
@@ -10,6 +16,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(expressLayouts);
+
+// implementing file upload program --start
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+  secret: 'CookingBlogSecretSession',
+  saveUninitialized: true,
+  resave: true
+}))
+app.use(flash());
+app.use(fileUpload());
+
+// implementing file upload program --end
+
+
 
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
